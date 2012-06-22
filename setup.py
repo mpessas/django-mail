330ab3906ca2a8d968dd8a0c2c6c8576cfb69287
Apostolos Bessas <mpessas@transifex.com>
2012-06-22 10:01:50 +0300

    Add setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="django-mail",
+    version='0.1a',
+    description="Define django mail templates.",
+    author="Apostolos Bessas",
+    author_email="mpessas@gmail.com",
+    packages=find_packages(),
+)
